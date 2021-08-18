981cf273be0534ee17d1c831a5fe94e1c20c258b
MarkusAleksander <markholden1989@googlemail.com>
2021-08-18 17:22:42 +0100

    correcting exports

--- src/js/View/drawDiscardDeck.js
+++ src/js/View/drawDiscardDeck.js
@@ -1,7 +1,9 @@
 import { Deck, View } from "./../Managers/managers";
 
-export default drawDiscardDeck = () => {
+const drawDiscardDeck = () => {
 
     let card = Deck.getDiscardDeck()[0];
     View.getDiscardDeck().innerHTML = View.createCardHTML(card, true);
 }
+
+export default drawDiscardDeck;
